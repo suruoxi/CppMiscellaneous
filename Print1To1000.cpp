f4d992baf5b2c20a4c0949d6f043f648fb0687f5
Peng Zhang <pzhangoo@mail.ustc.edu.cn>
2014-12-08 21:26:07 +0800

    Print 1 to 1000 without iter or judge state

---
+++ Print1To1000.cpp
@@ -0,0 +1,26 @@
+/*************************************************************************
+    > File Name: Print1To1000.cpp
+    > Author: Peng Zhang
+    > Mail: pengzhang_live@live.com 
+    > Created Time: 2014-12-08
+ ************************************************************************/
+/*
+ *http://coolshell.cn/articles/3445.html
+请把从1到1000的数打印出来，但你不能使用任何的循环语句或是条件语句。更不能写1000个printf或是cout。用C/C++语言
+ */ 
+
+#include<iostream>
+using namespace std;
+
+class Printer{
+public:
+	Printer(){
+		static unsigned i = 1;
+		cout<< i++ <<endl;
+	}
+};
+
+int main(){
+	Printer p[1000];
+	return 0;
+}
