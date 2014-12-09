54c1eb0802f61a6503c618f957886b57e8e4117b
Peng Zhang <pzhangoo@mail.ustc.edu.cn>
2014-12-09 10:03:07 +0800

    Trigraphs in c99. Just for fun. Don't use it.  Clang and gcc will ignore this be default. Use -trigraphs to enable it

---
+++ Trigraphs.cpp
@@ -0,0 +1,22 @@
+/*************************************************************************
+    > File Name: Trigraphs.cpp
+    > Author: Peng Zhang
+    > Mail: pengzhang_live@live.com 
+    > Created Time: 2014-12-09
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+
+int main(){
+	cout<<"??!"<<endl
+		<<"??="<<endl
+		<<"??("<<endl
+		<<"??)"<<endl
+		<<"??'"<<endl
+		<<"??<"<<endl
+		<<"??>"<<endl
+		<<"??-"<<endl;
+	return 0;
+
+}
