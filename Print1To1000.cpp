ff8284a0bb40e18162964186389875269ec75c5f
Peng Zhang <pzhangoo@mail.ustc.edu.cn>
2014-12-19 14:42:05 +0800

    some comments

--- Print1To1000.cpp
+++ Print1To1000.cpp
@@ -8,7 +8,9 @@
  *http://coolshell.cn/articles/3445.html
 请把从1到1000的数打印出来，但你不能使用任何的循环语句或是条件语句。更不能写1000个printf或是cout。用C/C++语言
  */ 
-
+/*
+ * 还有模板偏特化等方式可用
+ */ 
 #include<iostream>
 using namespace std;
 
