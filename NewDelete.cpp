08ef3b148ba1b9a77a112132100c383a1c86c66b
Peng Zhang <pzhangoo@mail.ustc.edu.cn>
2014-12-22 16:12:13 +0800

    TODO

---
+++ NewDelete.cpp
@@ -0,0 +1,11 @@
+/*************************************************************************
+    > File Name: NewDelete.cpp
+    > Author: Peng Zhang
+    > Mail: pengzhang_live@live.com 
+    > Created Time: 2014-12-22
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+
+
